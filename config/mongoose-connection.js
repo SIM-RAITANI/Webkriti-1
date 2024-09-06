0b74b79c8e6ab207e3e9a43520be9f990728096f
SIM_RAITANI <simranraitani53@gmail.com>
2024-09-07 00:48:18 +0530

    Project set todeployment

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb+srv://simranraitani9:simraitani3249@cluster0.dnw1w.mongodb.net/blog-database?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect("mongodb+srv://sujitraitani9:4JtyRHMEglY0oTOf@cluster0.nhaep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connected");
 
 }).catch((err)=>{
